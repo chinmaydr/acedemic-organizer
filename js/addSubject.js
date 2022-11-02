d7475d7e0c338eccfb6257eb79ae35abaf929b30
Azeem-Khan1 <aalookid@gmail.com>
2022-11-01 22:47:53 -0700

    Cards with user input (cards.html)

--- js/addSubject.js
+++ js/addSubject.js
@@ -10,12 +10,12 @@ function cancelForm() {
     document.getElementById("subjectPopup").style.display = "none";
   }
 
-  
+
 // Makes a file with subject name
 function submitForm() {
     document.getElementById("subjectPopup").style.display = "none";
 
     let subject = document.getElementById("submitBtn").value;
 
-    FileSystemDirectoryEntry.name = subject + ".html";
+    
   }
